fe131eb2f8dade70dcd72b2187409b3ed2719465
TundraClimate <tundra.tundere@gmail.com>
2024-04-02 11:45:17 +0900

    feat: add cmp deps

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -15,6 +15,13 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "hrsh7th/cmp-nvim-lsp-document-symbol",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-calc",
+      "uga-rosa/cmp-dictionary",
+      "saadparwaiz1/cmp_luasnip",
+      "ray-x/cmp-treesitter",
     },
     event = "InsertEnter",
     opts = function()
