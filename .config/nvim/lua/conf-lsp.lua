b43cd6af3d2c5086f486c506bd5435e9602e6c27
TundraClimate <tundra.tundere@gmail.com>
2024-04-15 18:19:40 +0900

    feat: enable autoformat

--- .config/nvim/lua/conf-lsp.lua
+++ .config/nvim/lua/conf-lsp.lua
@@ -29,7 +29,7 @@ local on_attach = function(_, bufnr)
   --buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   --buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   --buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap("n", "<A-l>", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  buf_set_keymap("n", "<C-s>", "<cmd>lua vim.lsp.buf.format()<CR><cmd>w<CR>", opts)
 end
 
 -- ★ これ
