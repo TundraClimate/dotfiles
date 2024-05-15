ebcf18fc70f7f74fc228d20be35509db39251ba8
TundraClimate <tundra.tundere@gmail.com>
2024-05-16 06:55:34 +0900

    feat: added install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd `dirname $0`
+
+echo create .zshrc symlink
+if [ ! -e ${HOME}/.zshrc ]; then
+  ln -s ${PWD}/.zshrc ${HOME}/.zshrc
+fi
+echo create .vimrc symlink
+if [ ! -e ${HOME}/.vimrc ]; then
+  ln -s ${PWD}/.vimrc ${HOME}/.vimrc
+fi
+echo create Neovim conf symlink
+if [ ! -d ${HOME}/.config/nvim ]; then
+  ln -s ${PWD}/.config/nvim ${HOME}/.config/nvim
+fi
