09b6e0e8940492313093377f2ce7dace673089b5
TundraClimate <tundra.tundere@gmail.com>
2024-07-01 03:14:39 +0900

    ✨feat: add nvim-scrollbar

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -11,5 +11,32 @@ return {
         -- Configuration here, or leave empty to use defaults
       })
     end
+  },
+  {
+    "petertriho/nvim-scrollbar",
+    event = "User AstroFile",
+    config = function()
+      require("scrollbar").setup({
+        handle = {
+          blend = 10,
+          color = "#226688",
+        },
+        marks = {
+          Search = {
+            text = "-",
+            color = "#F0F0F0"
+          },
+          Error = {
+            color = "#FF0000"
+          },
+          Warn = {
+            color = "#FFFF00"
+          },
+        },
+        handlers = {
+          cursor = false,
+        }
+      })
+    end
   }
 }
