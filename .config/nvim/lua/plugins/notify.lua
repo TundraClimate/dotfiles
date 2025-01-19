579693573542b562368fe4f2c77e93b7d27d9cf3
TundraClimate <tundra.tundere@gmail.com>
2025-01-19 22:03:52 +0900

    ✏️chore: union to ui.lua

--- .config/nvim/lua/plugins/notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "rcarriga/nvim-notify",
-  event = "BufEnter",
-  specs = {
-    { "nvim-lua/plenary.nvim", lazy = true },
-  },
-  opts = {}
-}
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,15 @@
 return {
+  {
+    "rcarriga/nvim-notify",
+    event = "BufEnter",
+    specs = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    opts = {}
+  },
+  {
     "catgoose/nvim-colorizer.lua",
     event = "BufReadPre",
     opts = {}
+  }
 }
