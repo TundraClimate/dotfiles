ff2a8a9a587933300387dd8b360f9bfca263a31c
TundraClimate <tundra.tundere@gmail.com>
2025-01-20 03:27:46 +0900

    ✨feat: add cmp

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+
+    opts = function()
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      local border_opts = {
+        border = "single",
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+      }
+      return {
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol",
+            maxwidth = {
+              menu = 50,
+              abbr = 50,
+            },
+
+            ellipsis_char = "...",
+            show_labelDetails = true,
+          }),
+        },
+        window = {
+          completion = cmp.config.window.bordered(border_opts),
+          documentation = cmp.config.window.bordered(border_opts),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "buffer",   priority = 250 },
+          { name = "path",     priority = 100 },
+        }),
+      }
+    end,
+  },
+}
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -5,22 +5,23 @@ return {
     specs = {
       { "nvim-lua/plenary.nvim", lazy = true },
     },
-    opts = {}
+    opts = {},
   },
   {
     "catgoose/nvim-colorizer.lua",
     event = "BufReadPre",
-    opts = {}
+    opts = {},
   },
   {
     "stevearc/dressing.nvim",
     event = "BufEnter",
-    opts = {}
+    opts = {},
   },
   {
     "onsails/lspkind.nvim",
-    event = "BufEnter",
+    event = "InsertEnter",
     opts = {
+      mode = "symbol",
       symbol_map = {
         Text = "󰉿",
         Method = "󰆧",
@@ -48,7 +49,8 @@ return {
         Operator = "󰆕",
         TypeParameter = "",
       },
-    }
+      menu = {},
+    },
   },
   {
     "lukas-reineke/indent-blankline.nvim",
@@ -65,21 +67,21 @@ return {
           "RainbowGreen",
           "RainbowViolet",
           "RainbowCyan",
-        }
-      }
+        },
+      },
     },
     config = function(_, opts)
-      local hooks = require "ibl.hooks"
+      local hooks = require("ibl.hooks")
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-          vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-          vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-          vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-          vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-          vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-          vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-          vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
       end)
       require("ibl").setup(opts)
     end,
-  }
+  },
 }
