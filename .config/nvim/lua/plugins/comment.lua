7855075aa539e10d377cd0f3fb47a0d93f269d79
TundraClimate <tundra.tundere@gmail.com>
2025-01-20 16:09:09 +0900

    ✨feat: add Comment

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,20 @@
+return {
+  "numToStr/Comment.nvim",
+  event = "BufEnter",
+  opts = {
+    toggler = {
+      line = "<leader>/",
+      block = "<leader>/",
+    },
+
+    opleader = {
+      line = "<leader>/",
+      block = "<leader>/",
+    },
+
+    mappings = {
+      basic = true,
+      extra = false,
+    },
+  },
+}
