96a5bd1edbfbfb5ec41b859009eca3a78f5e49f9
TundraClimate <tundra.tundere@gmail.com>
2025-01-20 15:17:49 +0900

    ✏️chore: change option

--- .config/nvim/lua/config/option.lua
+++ .config/nvim/lua/config/option.lua
@@ -33,32 +33,8 @@ vim.opt.swapfile = false
 vim.opt.wrap = true
 vim.opt.pumblend = 7
 vim.opt.cursorcolumn = true
-
-vim.cmd.colorscheme("nightfly")
-
--- ibl settings
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
 vim.opt.list = true
-
--- require("indent_blankline").setup {
---   space_char_blankline = " ",
---   show_current_context_start = true,
---   char_highlight_list = {
---     "IndentBlanklineIndent1",
---     "IndentBlanklineIndent2",
---     "IndentBlanklineIndent3",
---     "IndentBlanklineIndent4",
---     "IndentBlanklineIndent5",
---     "IndentBlanklineIndent6",
---   },
--- }
 
 vim.notify = require("notify")
 
-vim.g.icons_enabled = true
+vim.cmd.colorscheme("nightfly")
