eb6320ce3f544e7da6eed7f4db703f19d44d4f8d
TundraClimate <tundra.tundere@gmail.com>
2025-01-22 01:34:26 +0900

    ✨feat: improved cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -16,6 +16,40 @@ return {
         winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
       }
       return {
+        matching = {
+          disallow_fuzzy_matching = true,
+        },
+        sorting = {
+          comparators = {
+            function(entry1, entry2)
+              local prefix = "^" .. vim.api.nvim_get_current_line():match("%S+")
+              local match1 = entry1.completion_item.label:match(prefix)
+              local match2 = entry2.completion_item.label:match(prefix)
+
+              if match1 and not match2 then
+                return true
+              elseif match2 and not match1 then
+                return false
+              end
+
+              if entry1.source.name == "nvim_lsp" and entry2.source.name ~= "nvim_lsp" then
+                return true
+              elseif entry2.source.name == "nvim_lsp" and entry1.source.name ~= "nvim_lsp" then
+                return false
+              end
+
+              return nil
+            end,
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+        preselect = cmp.PreselectMode.None,
         formatting = {
           format = lspkind.cmp_format({
             mode = "symbol",
