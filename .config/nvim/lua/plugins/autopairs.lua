52c963ec8c787b72da07be9a9986e39503f28361
TundraClimate <tundra.tundere@gmail.com>
2025-01-21 23:50:09 +0900

    ✨feat: add cmp-autopair

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -2,4 +2,9 @@ return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
   opts = {},
+  config = function(opts)
+    require("nvim-autopairs").setup(opts)
+
+    require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+  end,
 }
