34818512e6afa6a186e2252f23419158712888cf
TundraClimate <tundra.tundere@gmail.com>
2025-01-25 23:36:00 +0900

    ✨feat: add some

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,16 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "BufEnter",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
       icons_enabled = true,
       theme = "auto",
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
     },
     sections = {
-      lualine_x = { 'encoding', 'filetype' },
-    }
-  }
+      lualine_x = { "encoding", "filetype" },
+    },
+  },
 }
