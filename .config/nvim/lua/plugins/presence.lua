ec86a2484740ed246427c897b8e2f6a7693f3313
TundraClimate <tundra.tundere@gmail.com>
2025-03-11 21:50:31 +0900

    ✨feat: Add presence.lua

---
+++ .config/nvim/lua/plugins/presence.lua
@@ -0,0 +1,14 @@
+return {
+  "andweeb/presence.nvim",
+  lazy = false,
+  opts = {
+    auto_update = true,
+    neovim_image_text = "Neovim",
+    main_image = "file",
+
+    editing_text = "Editing %s✏️",
+    file_explorer_text = "Browsing %s🌏",
+    reading_text = "Reading %s📖",
+    workspace_text = "Working on %s😎",
+  },
+}
