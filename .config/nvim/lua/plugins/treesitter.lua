7aede95ea568e0ee329ec4f90e9a81fab04585d4
TundraClimate <tundra.tundere@gmail.com>
2026-01-18 23:05:36 +0900

    ✨feat: Update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,47 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
-      { "m-demare/hlargs.nvim" },
-    },
-    event = "BufReadPre",
-    cmd = {
-      "TSBufDisable",
-      "TSBufEnable",
-      "TSBufToggle",
-      "TSDisable",
-      "TSEnable",
-      "TSToggle",
-      "TSInstall",
-      "TSInstallInfo",
-      "TSInstallSync",
-      "TSModuleInfo",
-      "TSUninstall",
-      "TSUpdate",
-      "TSUpdateSync",
-    },
-    build = ":TSUpdate",
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
-    opts = {
-      auto_install = false,
-      highlight = { enable = true },
-      incremental_selection = { enable = true },
-      indent = { enable = true },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true, branch = "main" },
+            { "m-demare/hlargs.nvim" },
         },
-      },
+        event = "BufReadPre",
+        cmd = {
+            "TSBufDisable",
+            "TSBufEnable",
+            "TSBufToggle",
+            "TSDisable",
+            "TSEnable",
+            "TSToggle",
+            "TSInstall",
+            "TSInstallInfo",
+            "TSInstallSync",
+            "TSModuleInfo",
+            "TSUninstall",
+            "TSUpdate",
+            "TSUpdateSync",
+        },
+        build = ":TSUpdate",
+        opts = {
+            auto_install = false,
+            highlight = { enable = true },
+            incremental_selection = { enable = true },
+            indent = { enable = true },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                },
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.config").setup(opts)
+            require("nvim-treesitter-textobjects.init").setup(opts)
+        end,
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    event = "BufRead",
-    opts = {},
-  },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "BufRead",
+        opts = {},
+    },
 }
