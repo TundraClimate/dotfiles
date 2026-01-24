5d0869bae58d1e0879347f2fe6ae07587c442ee6
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 20:22:59 +0900

    ✨feat: Change Keymap

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -12,13 +12,14 @@ keymap("n", "L", "10l")
 keymap("n", "<c-j>", "<C-f>")
 keymap("n", "<c-k>", "<C-b>")
 keymap("v", "j", "gj")
+keymap("v", "k", "gk")
 keymap("v", "H", "10h")
 keymap("v", "J", "10gj")
 keymap("v", "K", "10gk")
 keymap("v", "L", "10l")
 keymap("n", "<right>", "<cmd>bprevious<cr>")
 keymap("n", "<up>", function()
-  vim.api.nvim_buf_delete(0, { force = true })
+    vim.api.nvim_buf_delete(0, { force = true })
 end)
 keymap("n", "<C-e><C-e>", "f{a<enter><esc>ko")
 keymap("n", "??", "f;i?<esc>")
@@ -30,14 +31,14 @@ keymap("n", "&", "bi&<esc>")
 local telescope = require("telescope.builtin")
 
 local function has_git_changes()
-  local gitsigns = require("gitsigns")
+    local gitsigns = require("gitsigns")
 
-  local hunks = gitsigns.get_hunks()
-  if hunks and #hunks > 0 then
-    return true
-  else
-    return false
-  end
+    local hunks = gitsigns.get_hunks()
+    if hunks and #hunks > 0 then
+        return true
+    else
+        return false
+    end
 end
 
 keymap("n", "<leader>ff", telescope.find_files, {})
@@ -48,30 +49,30 @@ keymap("n", "<leader>fr", telescope.lsp_references, {})
 keymap("n", "<leader>gc", telescope.git_commits, {})
 keymap("n", "<leader>t", telescope.diagnostics, {})
 keymap("n", "<leader>gt", function()
-  if has_git_changes() then
-    telescope.git_status()
-  else
-    require("notify")("Buffer no changes")
-  end
+    if has_git_changes() then
+        telescope.git_status()
+    else
+        require("notify")("Buffer no changes")
+    end
 end, {})
 keymap("n", "<leader>gd", function()
-  if has_git_changes() then
-    local current_win = vim.api.nvim_get_current_win()
-    local wins_before = vim.api.nvim_list_wins()
+    if has_git_changes() then
+        local current_win = vim.api.nvim_get_current_win()
+        local wins_before = vim.api.nvim_list_wins()
 
-    require("gitsigns").diffthis()
+        require("gitsigns").diffthis()
 
-    vim.defer_fn(function()
-      local wins_after = vim.api.nvim_list_wins()
+        vim.defer_fn(function()
+            local wins_after = vim.api.nvim_list_wins()
 
-      for _, win in ipairs(wins_after) do
-        if not vim.tbl_contains(wins_before, win) and win ~= current_win then
-          vim.api.nvim_set_current_win(win)
-          break
-        end
-      end
-    end, 100)
-  else
-    require("notify")("Buffer no changes")
-  end
+            for _, win in ipairs(wins_after) do
+                if not vim.tbl_contains(wins_before, win) and win ~= current_win then
+                    vim.api.nvim_set_current_win(win)
+                    break
+                end
+            end
+        end, 100)
+    else
+        require("notify")("Buffer no changes")
+    end
 end)
