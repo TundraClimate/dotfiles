556154ec7b82bcb9e6dc525345972c6ab1263f4a
TundraClimate <tundra.tundere@gmail.com>
2026-02-05 22:27:12 +0900

    ✨feat: Apply ts_ls

--- .config/nvim/lsp/ts_ls.lua
+++ .config/nvim/lsp/ts_ls.lua
@@ -1,15 +1,15 @@
 return {
-  cmd = { "tsserver" },
-  filetypes = { "ts" },
-  on_attach = function(_, bufnr)
-    local function buf_set_keymap(...)
-      vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local opts = { noremap = true, silent = true }
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = { "typescript", "typescript.tsx" },
+    on_attach = function(_, bufnr)
+        local function buf_set_keymap(...)
+            vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+        local opts = { noremap = true, silent = true }
 
-    buf_set_keymap("n", "<leader>j", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "<A-enter>", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "<leader>RR", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<C-s>", "<cmd>lua vim.lsp.buf.format()<CR><cmd>w<CR>", opts)
-  end,
+        buf_set_keymap("n", "<leader>j", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        buf_set_keymap("n", "<A-enter>", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+        buf_set_keymap("n", "<leader>RR", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+        buf_set_keymap("n", "<C-s>", "<cmd>lua vim.lsp.buf.format()<CR><cmd>w<CR>", opts)
+    end,
 }
